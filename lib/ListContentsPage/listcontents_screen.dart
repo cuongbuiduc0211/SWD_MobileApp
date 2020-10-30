be00ccd8f65c430154490188ddb657691bafa455
TsunMoon <kimhexi1998@gmail.com>
2020-10-30 21:31:59 +0700

    fixed

--- lib/ListContentsPage/listcontents_screen.dart
+++ lib/ListContentsPage/listcontents_screen.dart
@@ -10,7 +10,7 @@ class ListContentsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(userLogin: userLogin,),
+      body: Body(),
     );
   }
 }
